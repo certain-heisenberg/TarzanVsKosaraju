d1b128cbdc9a51f3b0b9d645deacbf28cff91804
Kishan Kumar <kishan.k@vedantu.com>
2021-08-27 20:36:39 +0530

    Modify shell script

--- graph_tester.sh
+++ graph_tester.sh
@@ -1,12 +1,12 @@
 set -e
-g++ graph_gen.cpp -o graph_gen
-g++ kosaraju.cpp -o kosaraju
-g++ tarzan.cpp -o tarzan
+g++ -std=c++14 graph_gen.cpp -o graph_gen
+g++ -std=c++14 kosaraju.cpp -o kosaraju
+g++ -std=c++14 tarzan.cpp -o tarzan
 > kosaraju.txt
 > tarzan.txt
 for((i = 1; ; ++i)); do
-    ./graph_gen $i > int.txt
-    ./kosaraju < int.txt >> kosaraju.txt
-    ./tarzan < int.txt >> tarzan.txt
+    ./graph_gen $i > input.txt
+    ./kosaraju < input.txt >> kosaraju.txt
+    ./tarzan < input.txt >> tarzan.txt
     echo "Processed test: "  $i
 done
